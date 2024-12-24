422773ed96c0860c9b693ef4ab3965d0876805e2
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-12-24 10:49:40 -0600

    test stub

---
+++ service.spec.mjs
@@ -0,0 +1,8 @@
+import { ok } from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe('service', () => {
+  it('should start node-red', () => {
+    ok(true, 'node-red started')
+  })
+})
