86063104be362693af18c6ff902c256c533db05b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-12-27 16:27:09 -0600

    add helmet, add authn

--- service.mjs
+++ service.mjs
@@ -4,6 +4,9 @@ import { join } from 'node:path'
 import dotenv from 'dotenv'
 import RED from 'node-red'
 import express from 'express'
+import helmet from 'helmet'
+import { Strategy } from 'passport-google-oauth20'
+
 dotenv.config({
   path: process.env.DOTENV_CONFIG_PATH || join(process.cwd(), '.env')
 })
@@ -18,7 +21,45 @@ export function getNodeRedConfig () {
     httpAdminRoot: ADMIN_PATH,
     httpNodeRoot: API_PATH,
     userDir: join(process.cwd(), 'data'),
-    functionGlobalContext: { }
+    functionGlobalContext: { },
+    functionExternalModules: false,
+    externalModules: {
+      autoInstall: false,
+      palellete: {
+        allowInstall: false,
+        allowUpload: false,
+        allowList: [],
+        denyList: []
+      }
+    },
+    modules: {
+      allowInstall: false,
+      allowList: [],
+      denyList: []
+    },
+    adminAuth: {
+      type: 'strategy',
+      strategy: {
+        name: 'google',
+        label: 'Sign in with Google',
+        icon: 'fa-google',
+        strategy: Strategy,
+        options: {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: `${process.env.EXTERNAL_ORIGION ?? 'http://localhost:3000'}${API_PATH}auth/google/callback`
+        },
+        verify: (_accessToken, _refreshToken, profile, cb) => {
+          cb(null, profile)
+        }
+      },
+      users: (process.env.ALLOWED_USERS ?? '').split(',').map((username) => {
+        return {
+          username,
+          permissions: ['*']
+        }
+      })
+    }
   }
 }
 
@@ -26,6 +67,14 @@ const settings = getNodeRedConfig()
 const app = express()
 const server = createServer(app)
 RED.init(server, settings)
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      'script-src': ["'self'", "'unsafe-inline'"],
+      'script-src-attr': ["'self'", "'unsafe-inline'"]
+    }
+  }
+}))
 app.use(settings.httpAdminRoot, RED.httpAdmin)
 app.use(settings.httpNodeRoot, RED.httpNode)
 
